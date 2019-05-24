874bc18255d58c5190f3d931a2f1f291b348b16c
Salman Rahman <salman@Salmans-MacBook-Pro.local>
2019-05-24 22:06:06 +0600

    build

---
+++ build/precache-manifest.9d9115dd0ada8690a90d38cad18b48bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e316a336037b6f16a7f2e51972958cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48339c2e72056be8667b",
+    "url": "/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "ea38ba0780e6a9824b3f",
+    "url": "/static/css/main.645e54ef.chunk.css"
+  },
+  {
+    "revision": "48339c2e72056be8667b",
+    "url": "/static/js/2.d4c03dbd.chunk.js"
+  },
+  {
+    "revision": "ea38ba0780e6a9824b3f",
+    "url": "/static/js/main.16c409a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
